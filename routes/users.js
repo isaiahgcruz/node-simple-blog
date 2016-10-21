050d96b35e2c8617611a84029826ccd248a58c87
Isaiah Cruz <isaiahgcruz@gmail.com>
2016-10-21 11:20:53 +0800

    Added blogs route
    - changed var to const
    - fixed a search query

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../models/user');
-var jwt_decode = require('jwt-decode');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const jwt_decode = require('jwt-decode');
+const Blog = require('../models/blog');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -29,8 +30,17 @@ router.get('/me', function (req, res) {
   return res.send(response);
 });
 
+router.get('/:id/blogs', function(req, res) {
+  Blog.find().sort({ createdAt: -1 }).populate('_user').find({_user: req.params.id }).exec(function (err, blogs) {
+    if (err) {
+        return res.status(500).send(err);
+    }
+    res.json({ blogs });
+  });
+})
+
 router.get('/:id', function (req, res) {
-  User.findOne({ id: req.params.id }).exec(function (err, user) {
+  User.findOne({ _id: req.params.id }).exec(function (err, user) {
     if (err) {
       res.status(500).send(err);
     }
